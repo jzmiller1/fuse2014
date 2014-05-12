f3e47c4c19ce658ff251ed0432f8d4f372adaa7b
Crystal Lyliston <crlyli0476@ung.edu>
2014-05-12 16:39:36 -0400

    Created initial models, tests and initial_data fixture.
    Created initial implementation of Cemetery, Marker, Person and MarkerImage models to hold the data for the project and registered them in the admin. Each model (except MarkerImage) has a test for the __str__ method. An initial_data (JSON) fixture containing one Cemetery, five Markers and eight Persons was also created but does not yet contain MarkerImage data.  Models do not contain any geographic data.  Geographic data will be implemented in the future.

--- cemeteries/models.py
+++ cemeteries/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class Cemetery(models.Model):
+    """ Cemetery model.
+
+    Represents a cemetery.
+
+    """
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+
+    def __str__(self):
+        return "{} Cemetery".format(self.name)
+
+
+class Marker(models.Model):
+    """ Marker Model.
+
+    Represents a cemetery marker (gravestone).
+
+    """
+    cemetery = models.ForeignKey(Cemetery)
+    markerid = models.IntegerField(primary_key=True)
+    condition = models.CharField(max_length=15)
+    readable = models.BooleanField(default=None)
+    epitaph = models.CharField(max_length=20)
+    family_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "Markerid: {} - {}".format(self.markerid, self.cemetery)
+
+
+class Person(models.Model):
+    """ Person Model.
+
+    Represents a person interred at a cemetery and connects them to their Marker.
+
+    """
+
+    markerid = models.ForeignKey(Marker)
+    full_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=7)
+    veteran = models.CharField(max_length=7)
+    b_year = models.CharField(max_length=4)
+    d_year = models.CharField(max_length=4)
+    epitaph = models.TextField()
+    footstone = models.BooleanField(default=None)
+    footstoneI = models.TextField()
+
+    def __str__(self):
+        return "FullName : {0}, {1}".format(self.full_name, self.markerid)
+
+
+class MarkerImage(models.Model):
+    """ MarkerImage Model
+
+    Represents an image of a marker.
+
+    """
+    markerid = models.ForeignKey(Marker)
+    image = models.ImageField()
+
+    def __str__(self):
+        return "{}".format(self.markerid)
--- cemeteries/tests.py
+++ cemeteries/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from cemeteries import models
+
+
+class MarkerTestCase(TestCase):
+    def setUp(self):
+        pass
+        # Tests run against initial_data.json fixture data so creating test data within the test is not needed.
+        # models.Cemetery.objects.create(name="test_cem", description="test")
+        # models.Marker.objects.create(cemetery=models.Cemetery.objects.get(name="test_cem"),
+        #                              markerid=6027,
+        #                              condition="Aged",
+        #                              readable=True,
+        #                              epitaph="RIP",
+        #                              family_name="Tester",
+        #                             )
+
+    def test_marker_string(self):
+        """Verifying __str__ returns with proper formatting."""
+        marker = models.Marker.objects.get(markerid=6027)
+        self.assertEqual(marker.__str__(), "Markerid: 6027 - Timberidge Cemetery")
+
+
+class CemeteryTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_cemetery_string(self):
+        """Verifying __str__ returns with proper formatting."""
+        cemetery = models.Cemetery.objects.get(pk=1)
+        self.assertEqual(cemetery.__str__(), "Timberidge Cemetery")
+
+
+class PersonTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_person_string(self):
+        """Verifying __str__ returns with proper formatting."""
+        person = models.Person.objects.get(first_name="Alonzo")
+        self.assertEqual(person.__str__(), "FullName : Alonzo Poole, Markerid: 6006 - Timberidge Cemetery")
