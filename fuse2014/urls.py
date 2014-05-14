b0e0add9e562ccd6c906f065532d00b6e813e8fc
Crystal Lyliston <crlyli0476@ung.edu>
2014-05-14 10:20:52 -0400

    Added login required and commiting settings files.
    Added login required to the urls for both list and detail views for cemeteries, marker and person. Added the required settings in settings.py for django-registration.

--- fuse2014/urls.py
+++ fuse2014/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cemeteries.urls', namespace='cemeteries')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
