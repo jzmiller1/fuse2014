470bec260ac3eec84fdc99b74a6f5949d51c0675
Crystal Lyliston <crlyli0476@ung.edu>
2014-06-05 11:55:59 -0400

    Deleted Login Required
    Removed Login Required on all urls and commited links on nav bar for log in and register. Login was stuck in a loop when added to server.

--- cemeteries/urls.py
+++ cemeteries/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from .views import MainView, CemeteryListView, CemeteryDetailView, MarkerListView,MarkerDetailView
 from .views import PersonListView, PersonDetailView,AboutView, SymbologyView, PeopleView, MarkerMapView,PersonMapView, SymbolMapView
 from .models import Cemetery, Marker, Person, Symbology
-from django.contrib.auth.decorators import login_required
-from djgeojson.views import GeoJSONLayerView
+#from django.contrib.auth.decorators import login_required
+
 
 
 urlpatterns = patterns('',
@@ -13,15 +13,15 @@ urlpatterns = patterns('',
 
     url(r'^main$', MainView.as_view(), name='main'),
     url(r'^about/$', AboutView.as_view(), name='about'),
-    url(r'^cemeteries/$', login_required(CemeteryListView.as_view(model=Cemetery)), name='cem_lview'),
-    url(r'^cemeteries/(?P<pk>\d+)/$', login_required(CemeteryDetailView.as_view()), name='cem_dview'),
-    url(r'^marker/$', login_required(MarkerListView.as_view(model=Marker)), name='marker_lview'),
-    url(r'^marker/(?P<pk>\d+)/$', login_required(MarkerDetailView.as_view()),name='marker_dview'),
-    url(r'^person/$', login_required(PersonListView.as_view(model=Person)), name='person_lview'),
-    url(r'^person/(?P<pk>\d+)/$', login_required(PersonDetailView.as_view()), name='person_dview'),
-    url(r'^people_search/$', login_required(PeopleView.as_view()), name='people_view'),
-    url(r'^symbology/$', login_required(SymbologyView.as_view(model=Symbology)), name='symbology'),
-    url(r'^map_marker/$', login_required(MarkerMapView.as_view()), name='markermap'),
-    url(r'^map_person/$', login_required(PersonMapView.as_view()), name='personmap'),
-    url(r'^symbolmap/(?P<pk>\d+)/$', login_required(SymbolMapView.as_view()), name='symbolmap'),
+    url(r'^cemeteries/$', CemeteryListView.as_view(model=Cemetery), name='cem_lview'),
+    url(r'^cemeteries/(?P<pk>\d+)/$', CemeteryDetailView.as_view(), name='cem_dview'),
+    url(r'^marker/$', MarkerListView.as_view(model=Marker), name='marker_lview'),
+    url(r'^marker/(?P<pk>\d+)/$', MarkerDetailView.as_view(), name='marker_dview'),
+    url(r'^person/$', PersonListView.as_view(model=Person), name='person_lview'),
+    url(r'^person/(?P<pk>\d+)/$', PersonDetailView.as_view(), name='person_dview'),
+    url(r'^people_search/$', PeopleView.as_view(), name='people_view'),
+    url(r'^symbology/$', SymbologyView.as_view(model=Symbology), name='symbology'),
+    url(r'^map_marker/$', MarkerMapView.as_view(), name='markermap'),
+    url(r'^map_person/$', PersonMapView.as_view(), name='personmap'),
+    url(r'^symbolmap/(?P<pk>\d+)/$', SymbolMapView.as_view(), name='symbolmap'),
 )
