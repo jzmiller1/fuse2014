dded645fda4e0c99a4cc155379115f28a6d9c254
jzmiller1 <zac.miller@ung.edu>
2014-07-22 18:24:58 -0400

    Add robots.txt

--- fuse2014/urls.py
+++ fuse2014/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.sitemaps import GenericSitemap
+from django.http import HttpResponse
 from fuse2014.sitemaps import MainSitemap
 from cemeteries.models import Person, Cemetery
 
@@ -11,7 +12,6 @@ sitemaps = {
     'people': GenericSitemap({'queryset': Person.objects.all(), }, changefreq='monthly', priority=0.6),
     'cemeteries': GenericSitemap({'queryset': Cemetery.objects.all(), }, changefreq='monthly', priority=0.6),
 }
-
 
 urlpatterns = patterns('',
     # Examples:
@@ -26,6 +26,7 @@ urlpatterns = patterns('',
     url(r'^sitemap\.xml$',
         'django.contrib.sitemaps.views.sitemap',
         {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap')
+        name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /api/", content_type="text/plain"))
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
